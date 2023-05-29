60b07cae5645633b8f75f334ae9d093287e5d9d6
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-05-29 11:21:16 +0700

    login register ci ajax | Dashboard

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -2,25 +2,30 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Dashboard extends CI_Controller {
+
     public function __construct() {
+        
         parent::__construct();
 
-        // cek session login
-        if ($this->session->userdata('id_user') != "") {
+        //cek session login
+        if($this->session->userdata("id_user") == "") {
             redirect('/login');
+        }
+
     }
-}
 
-public function index() {
-    // load view from login
-    $this->load->view('dashboard');
-}
+    public function index()
+    {
+        //load view form login
+        $this->load->view('dashboard');
+    }
 
-public function logout() {
-    // hapus session
-    $this->session->sees_destroy();
+    public function logout()
+    {
+        //hapus session
+        $this->session->sess_destroy();
 
-    redirect('/login');
+        redirect('/login');
+    }
+
 }
-}
-?>
