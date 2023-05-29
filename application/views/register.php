52287c328e1741dca164d75ad19d50964b20b5fa
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-05-29 09:58:26 +0700

    login register ci ajax

--- application/views/register.php
+++ application/views/register.php
@@ -1,123 +1,144 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
 
     <title>Register Akun</title>
-</head>
-<body>
-    
-<div class="container" style="margin-top: 50px">
-<div class="row">
-    <div class="col-md-5 offset-md-3">
-        <div class="card">
-            <div class="card-body">
-                <label>REGISTER</label>
-                <hr>
+  </head>
+  <body>
 
-                <div class="form-group">
-                    <label>Nama Lengkap</label>
-                    <input type="text" class="form-control" id="nama_lengkap" placeholder="Masukkan Nama Lengkap">
-                </div>
+    <div class="container" style="margin-top: 50px">
+      <div class="row">
+        <div class="col-md-5 offset-md-3">
+          <div class="card">
+            <div class="card-body">
+              <label>REGISTER</label>
+              <hr>
                 
                 <div class="form-group">
-                    <label>Username</label>
-                    <input type="text" class="form-control" id="username" placeholder="Masukkan Username">
-                </div>
-               
-                <div class="form-group">
-                    <label>Password</label>
-                    <input type="password" class="form-control" id="password" placeholder="Masukkan Password">
+                  <label>Nama Lengkap</label>
+                  <input type="text" class="form-control" id="nama_lengkap" placeholder="Masukkan Nama Lengkap">
                 </div>
 
+                <div class="form-group">
+                  <label>Username</label>
+                  <input type="text" class="form-control" id="username" placeholder="Masukkan Username">
+                </div>
+
+                <div class="form-group">
+                  <label>Password</label>
+                  <input type="password" class="form-control" id="password" placeholder="Masukkan Password">
+                </div>
+                
                 <button class="btn btn-register btn-block btn-success">REGISTER</button>
-</div>
-</div>
-<div class="text-center" style="margin-top: 15px">
+              
+            </div>
+          </div>
+
+          <div class="text-center" style="margin-top: 15px">
             Sudah punya akun? <a href="<?php echo base_url() ?>index.php/login">Silahkan Login</a>
           </div>
+
+        </div>
+      </div>
     </div>
-</div>
-</div>
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" ></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/8.11.8/sweetalert2.all.min.js"></script>
 
     <script>
-        $(document).ready(function(){
-            $('.btn-register').click(function(){
-                var nama_lengkap = $('#nama_lengkap').val();
-                var username = $('#username').val();
-                var password = $('#password').val();
+      $(document).ready(function() {
 
-                if (nama_lengkap.length == "") {
-                    swal.fire({
-                        type: 'warning',
-                        title: 'opps..',
-                        text: 'Nama lengkap wajib diisi !'
-                    });
+        $(".btn-register").click( function() {
+        
+          var nama_lengkap = $("#nama_lengkap").val();
+          var username = $("#username").val();
+          var password = $("#password").val();
 
-                } else if(username.length == "") {
-                    swal.fire({
-                        type: 'warning',
-                        title: 'opps..',
-                        text: 'Nama lengkap wajib diisi !'
-                    });
+          if (nama_lengkap.length == "") {
 
-                } else if(password.length == "") {
-                    swal.fire({
-                        type: 'warning',
-                        title: 'opps..',
-                        text: 'Nama lengkap wajib diisi !'
-                    });
-                    } else {
-                        //ajax
-                        $.ajax({
-
-                            url: "<?php echo base_url() ?>index.php/register/simpam",
-                            type: "POST",
-                            data: {
-                                "nama_lengkap": nama_lengkap,
-                                "username": username,
-                                "password": password
-                            },
-
-                            success:function(response) {
-                                if (response == "success") {
-                                    swal.fire({
-                                        type: 'success',
-                                        title: 'Register Berhasil',
-                                        text: 'Silahkan login!'
-                                });
-
-                                $("#nama_lengkap").val('');
-                                $("#username").val('');
-                                $("#password").val('');
-                            } else {
-                                swal.fire ({
-                                    type: 'error',
-                                    title: 'Register Gagal!',
-                                    text: 'Silakan coba lagi!'
-                                });
-                            }
-                            console.log(response);
-                        },
-
-                        error: function(response) {
-                            swal.fire({
-                                type: 'error',
-                                title: 'Opps!',
-                                text: 'server error!'
-                            });
-                        }
-                    })
-                }
+            Swal.fire({
+              type: 'warning',
+              title: 'Oops...',
+              text: 'Nama Lengkap Wajib Diisi !'
             });
-            }); 
+
+          } else if(username.length == "") {
+
+            Swal.fire({
+              type: 'warning',
+              title: 'Oops...',
+              text: 'Username Wajib Diisi !'
+            });
+
+          } else if(password.length == "") {
+
+            Swal.fire({
+              type: 'warning',
+              title: 'Oops...',
+              text: 'Password Wajib Diisi !'
+            });
+
+          } else {
+
+            //ajax
+            $.ajax({
+
+              url: "<?php echo base_url() ?>index.php/register/simpan",
+              type: "POST",
+              data: {
+                  "nama_lengkap": nama_lengkap,
+                  "username": username,
+                  "password": password
+              },
+
+              success:function(response){
+
+                if (response == "success") {
+
+                  Swal.fire({
+                    type: 'success',
+                    title: 'Register Berhasil!',
+                    text: 'silahkan login!'
+                  });
+
+                  $("#nama_lengkap").val('');
+                  $("#username").val('');
+                  $("#password").val('');
+
+                } else {
+
+                  Swal.fire({
+                    type: 'error',
+                    title: 'Register Gagal!',
+                    text: 'silahkan coba lagi!'
+                  });
+
+                }
+
+                console.log(response);
+
+              },
+
+              error:function(response){
+                  Swal.fire({
+                    type: 'error',
+                    title: 'Opps!',
+                    text: 'server error!'
+                  });
+              }
+
+            })
+
+          }
+
+        }); 
+
+      });
     </script>
-</body>
+
+  </body>
 </html>
